7177f192544f3c478fbb4ff6a009f5de520167c0
arikaran03 <arikaran258@gmail.com>
2025-03-19 09:31:42 +0530

    add gitignore

--- check.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBnScEqDwpc65eGcNz7Ogai9hO35gv9CP8")
-
-model = genai.GenerativeModel("gemini-1.5-pro-latest")  # Use a valid model
-response = model.generate_content("Hello, how are you?")
-print(response.text)
